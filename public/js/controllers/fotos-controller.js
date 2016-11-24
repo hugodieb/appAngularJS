d9a3f313f021811a65bc3ca3b85c638f43d76948
Hugo <hugodieb.hd@gmail.com>
2016-11-24 11:23:52 -0200

    adicao do angular na aplicacao

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,7 @@
+angular.module('appangular').controller('FotoController', function($scope){
+   
+    $scope.foto = {        
+        titulo : "Gatinho",
+        url : "http://situado.net/fotos/2009/10/Animais-1.jpg"         
+    };    
+});
