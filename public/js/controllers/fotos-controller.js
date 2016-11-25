0ce12b8a32b53544de0ee8e4da6240254932b87b
Hugo <hugodieb.hd@gmail.com>
2016-11-25 09:18:05 -0200

    uso do ng-repeat

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,7 +1,43 @@
-angular.module('appangular').controller('FotoController', function($scope){
-   
-    $scope.foto = {        
-        titulo : "Gatinho",
-        url : "http://situado.net/fotos/2009/10/Animais-1.jpg"         
-    };    
+//neste caso aqui quer indicar um modulo ja existente, no caso um controller
+angular.module('appangular').controller('FotoController', function($scope, $http){//$resource) {
+//   var fotos = [
+//       {
+//        titulo:'Leaozinho',
+//        url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+//       },
+//       {
+//        titulo:'Leaozinho2',
+//        url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+//       },
+//       {
+//        titulo:'Leaozinho3',
+//        url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+//       },
+//       {
+//        titulo:'Leaozinho4',
+//        url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
+//       },
+// ];
+    
+    $scope.fotos =[];
+    
+    
+  //OK
+    /*
+    var promise = $http.get('v1/fotos');
+    promise.then(function(retorno)  {
+        fotos = retorno.data;
+        $scope.fotos = fotos;
+    }).catch(function(error) {
+        console.log(error);
+    });
+    */
+    
+    $http.get('v1/fotos')
+    .success(function(fotos){
+        $scope.fotos = fotos
+    })
+    .error(function(erro){
+        console.error(erro);
+    })
 });
